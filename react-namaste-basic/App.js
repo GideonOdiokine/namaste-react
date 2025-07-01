35c0f38756ae09a83bc13fd4b63c27d56e419d52
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-07-02 00:18:03 +0100

    feat: setup react using cdn links and created nested HTML structure

---
+++ react-namaste-basic/App.js
@@ -0,0 +1,20 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+
+const element = React.createElement(
+  "h1",
+  {
+    id: "heading",
+  },
+  "Namaste React"
+);
+
+ReactDOM.render(parent, document.getElementById("root"));
