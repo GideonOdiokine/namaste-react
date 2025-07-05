a38dd758a49c642553e993d66dadfab2dd2b8df3
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-07-06 00:28:32 +0100

    update gitignore

--- react-namaste-basic/App.js
+++ react-namaste-basic/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child" },  [
     React.createElement("h1", {}, "I am h1 tag"),
     React.createElement("h2", {}, "I am h2 tag"),
+
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am h1 tag"),
@@ -17,4 +21,6 @@ const element = React.createElement(
   "Namaste React"
 );
 
-ReactDOM.render(parent, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+
+root.render(element);
