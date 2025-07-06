f793640718b759a2f95b67f58779fc4ec87bf320
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-07-06 12:24:42 +0100

    learn about react.createElement and JSX Syntax

--- react-namaste-basic/App.js
+++ react-namaste-basic/App.js
@@ -1,26 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" },  [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+// React Element
+const heading = React.createElement("h1", { id: "heading" }, "Hello React!!");
 
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+// JSX
+const jsxHeading = <h1 id='heading'>Namaste React using JSX</h1>
 
-const element = React.createElement(
-  "h1",
-  {
-    id: "heading",
-  },
-  "Namaste React"
-);
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(element);
+root.render(jsxHeading);
+
+
