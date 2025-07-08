35492039514ebef3951944be4cf6a22a5d80e79c
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-07-08 14:51:41 +0100

    learn about react component

--- react-namaste-basic/App.js
+++ react-namaste-basic/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 // React Element
-const heading = React.createElement("h1", { id: "heading" }, "Hello React!!");
+// const heading = React.createElement("h1", { id: "heading" }, "Hello React!!");
 
 // JSX
-const jsxHeading = <h1 id='heading'>Namaste React using JSX</h1>
+// const jsxHeading = <h1 id='heading'>Namaste React using JSX</h1>
 
+
+const HeadingComponent = ()=>(
+    <div className='bg-black'>
+        Hello girl
+    </div>
+)
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
 
 
